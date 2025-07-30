22324e32a8bb87dceb751479014ec77d69783c43
Harishthokka <harishthokka007@gmail.com>
2025-07-30 11:51:05 +0530

    Create Type.java

---
+++ Type.java
@@ -0,0 +1,48 @@
+class Primitive Datatype
+
+byte byte var;
+
+Short Short Vair
+
+long longvar;
+
+int int var;
+
+boolean bool varsie
+
+blo
+
+float float Var
+
+double double var;
+
+char charvar,
+
+Nold get Default values(,)
+
+System.out.println("byte default val: "+ bytelas);
+
+System.out.println("short default val: "+ short van);
+
+System; out.printh (int default val: "thitvay)
+
+System.out.println("long default val: "+long Van)
+
+System.out.println("float default val: " + float Ver)
+
+System.out.println("double default val:" + double Var),
+
+"System.out.println("char default val: "t charvar)
+
+System.out.println("boolean default val. "+ bod var);
+
+५
+public clar Default Values
+
+public static void main(String args[])
+
+Primitive Datatype P= new primitive Data type ();
+
+P.getDefaultValues();
+
+3

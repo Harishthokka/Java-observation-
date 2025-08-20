9fc4f3617745eea1f3ad0e8c3bdb429a47c31034
Harishthokka <harishthokka007@gmail.com>
2025-08-20 11:08:32 +0530

    Add files via upload

--- Ragged.java
+++ Ragged.java
@@ -1,24 +1,31 @@
-//program for array operations 
+//program for ragged array  
 import java.util.Scanner;
 import java.util.Arrays;
 class Ragged
 {
 	public static void main(String[] args)
 	{
-		int[][] raggedarr=new int[4][];
+		int[][] raggedarr=new int[3][];
 		raggedarr[0]=new int[4];
 		raggedarr[1]=new int[2];
 		raggedarr[2]=new int[3];
-		raggedarr[3]=new int[4];
 		Scanner sc=new Scanner(System.in);
 		for(int i=0;i<raggedarr.length;i++)
 		{
-		        for(int j=0;j<raggedarr.length;j++)
+		        for(int j=0;j<raggedarr[i].length;j++)
 		        {
-		                System.out.println("enter ele["+i+"]["]);
+		                System.out.println("enter ele["+i+"]["+j+"]");
 		                raggedarr[i][j]=sc.nextInt();
 		        }
 		}
-		System.out.println("ragged array "+Arrays.toString(raggedarr));
+		System.out.println("ragged array is");
+		for(int i=0;i<raggedarr.length;i++)
+		{
+		        for(int j=0;j<raggedarr[i].length;j++)
+		        {
+		                System.out.print(raggedarr[i][j]);
+		        }
+	        System.out.println();
+		}
 	}
 }
